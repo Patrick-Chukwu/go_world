d1a278d13711ac666ab1239eb279005eff86b6d9
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2025-11-02 19:50:18 +0100

    feat update dile

--- for_loop/solution5.go
+++ for_loop/solution5.go
@@ -1,3 +1,18 @@
 package forloop
 
-impotrt
+import "fmt" 
+
+func ForFive() {
+	loop := [][]int{
+		{	1, 3, 5 },
+		{	4, 5, 6 },
+		{	2, 4, 6 },
+	}
+
+	for i, j := range loop {
+	fmt.Printf("The index of  %v is %v \n", j, i)
+	for p, inside := range j {
+		fmt.Printf("The index of  %v is %v \n", inside, p)
+	}
+	}
+}
--- main.go
+++ main.go
@@ -9,4 +9,5 @@ import (
 func main() {
 	fmt.Println(quote.Go())
 	forloop.ForOne()
+	forloop.ForFive()
 }
